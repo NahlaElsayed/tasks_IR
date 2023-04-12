d45225a69035c44b29980d97f73becb13e305191
NahlaElsayed <110737762+NahlaElsayed@users.noreply.github.com>
2023-04-13 01:13:47 +0200

    Delete task/task3 directory

--- task/task3/section3 pouns.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-df = pd.read_csv('top 100 streamed songs.csv')
-num_null_rows = df.isnull().sum().sum()
-print("Number of null rows before removal:", num_null_rows)
-df.dropna(inplace=True)
-num_null_rows = df.isnull().sum().sum()
-print("Number of null rows after removal: ", num_null_rows)
